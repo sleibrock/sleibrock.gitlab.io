d85e6817f57630830c1e9cd88d327cdf6f5d4907
Steven Leibrock <steven.leibrock@gmail.com>
2019-08-29 23:05:28 -0400

    New page style/index

--- tasks/index.rkt
+++ tasks/index.rkt
@@ -10,24 +10,28 @@
     (meta ([description "Steven Leibrock's personal website"])))
    
    (body
-    (article
-      (h1 "Steven's Site")
-      (section
-       (p "Welcome to the personal homepage of Steven Leibrock")
-       (p "For the most part there is nothing really stored here... For now.")
-       
-       (h2 "Code")
-       (p "I upload all my code to two sources:")
-       ,(unordered-list `(,(link-to "GitHub" "https://github.com/sleibrock")
-                          ,(link-to "GitLab" "https://gitlab.com/sleibrock")))
+    (div ([id "container"])
+         (div ([id "navbar"])
+              (span ([id "navtitle"]) "Steven's Site")
+              (span ([class "navlink"]) "Test1")
+              (span ([class "navlink"]) "Test2"))
 
-       (h2 "Computer Builds")
-       (p ,(link-to "Click here" "builds.html") " to see all of my computer builds")
-       ))
-    
-    (hr)
-    (footer
-     (nav
-      (p "Steven Leibrock 2019"))))))
+         (div ([id "content"])
+              (article
+               (section
+                (p "Welcome to the personal homepage of Steven Leibrock")
+                (p "For the most part there is nothing really stored here... For now.")
+                
+                (h2 "Code")
+                (p "I upload all my code to two sources:")
+                ,(unordered-list `(,(link-to "GitHub" "https://github.com/sleibrock")
+                                   ,(link-to "GitLab" "https://gitlab.com/sleibrock")))
+                
+                (h2 "Computer Builds")
+                (p ,(link-to "Click here" "builds.html") " to see all of my computer builds")
+                )))
+         
+         (div ([id "footer"])
+              (p "Steven Leibrock 2019"))))))
 
 (xexpr->file (current-template) "public/index.html") 
